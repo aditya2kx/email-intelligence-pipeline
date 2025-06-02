23cc191046fe5376230b53cef8367a8c9b08b021
Aditya Parikh <aditya.2ky@gmail.com>
2025-06-01 22:20:47 -0500

    Update auth files for robustnes of secrets

--- src/gmail/auth.py
+++ src/gmail/auth.py
@@ -1,1 +1,27 @@
 # Auth module
+
+import os
+from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+load_dotenv()
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+CREDENTIALS_PATH = os.getenv('GMAIL_CREDENTIALS_PATH', 'credentials.json')
+TOKEN_PATH = os.getenv('GMAIL_TOKEN_PATH', 'token.json')
+
+def authenticate():
+    creds = None
+    if os.path.exists(TOKEN_PATH):
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN_PATH, 'w') as token:
+            token.write(creds.to_json())
+    return creds
