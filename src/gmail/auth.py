4a35802cf7be456b22a410d355528a6e59d55f90
Aditya Parikh <aditya.2ky@gmail.com>
2025-06-01 22:40:49 -0500

    Fix: Ignore venv and secure auth with dotenv

--- src/gmail/auth.py
+++ src/gmail/auth.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 
 load_dotenv()
 
